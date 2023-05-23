5d8f15e02c7f4831ddd8c00dffa1cd5280e57b8e
Anton Korzhov <statser@mail.ru>
2023-05-23 21:07:46 +0300

    add example 5 masha

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Insert username: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Hey MASHA!!!");
+}
+else
+{
+Console.Write("Hi, ");
+Console.WriteLine(username);
+}
