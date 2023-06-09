22a656d25aacaa339b356feed38e40e260b06050
Anton Korzhov <statser@mail.ru>
2023-06-09 18:32:32 +0300

    Lesson 5 add tasks and see the video

---
+++ Les5_LangProg/Task33/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int Insert(string s)
+{
+    Console.Write(s);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] GetArrayRandom(int q)
+{
+    int[] array = new int[q];
+    for (int i = 0; i < q; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    return array;
+}
+string FindInArray(int[] array, int digit)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == digit) return "ДА";
+    }
+    return "НЕТ";
+}
+
+int[] a = GetArrayRandom(Insert("Введите количество элементов массива: "));
+int d = Insert("Загадайте число: ");
+// Console.Write($"{ArrayToStr(a)} => {TakeZeroRedLineSum(a)[0]} +{TakeZeroRedLineSum(a)[1]}");
+Console.WriteLine($"{d}: массив [{String.Join(",", a)}] -> {FindInArray(a, d)}");
