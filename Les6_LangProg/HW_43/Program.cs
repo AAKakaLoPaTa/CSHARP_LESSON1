d267aacf8c7766ab7e666e6caffd52f12c6e5307
Anton Korzhov <statser@mail.ru>
2023-06-09 22:36:16 +0300

    Les6 переделал 43 задачу

--- Les6_LangProg/HW_43/Program.cs
+++ Les6_LangProg/HW_43/Program.cs
@@ -1,19 +1,23 @@
-﻿double Insert(string s)
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double Insert(string s)
 {
     Console.Write(s);
     return Convert.ToInt32(Console.ReadLine());
 }
 double[] Peres(double b1, double k1, double b2, double k2)
 {
-    double[] array = new double[2];
+    double[] array = new double[3];
     array[0] = (b1 - b2) / (k2 - k1);
     array[1] = k1 * array[0] + b1;
+    array[2] = k2 * array[0] + b2;
     return array;
 }
-
+// Не учел возможность параллельности прямых - проверять равность двух уравнений нужно!
 double b1 = Insert("Введите b1: ");
 double k1 = Insert("Введите k1: ");
 double b2 = Insert("Введите b2: ");
 double k2 = Insert("Введите k2: ");
 double[] p = Peres(b1, k1, b2, k2);
-Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({p[0]}; {p[1]})");
+if (p[1] != p[2]) {Console.WriteLine($"Прямые параллельны");}
+else {Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({p[0]}; {p[1]})");}
